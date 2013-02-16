fa90e58436b9b9e51bddfcb6db88d2de260df3ad
Sagar Mehta <mehta.sagarv@gmail.com>
2013-02-15 16:02:04 -0800

    added contact schema

---
+++ schema/contacts.js
@@ -0,0 +1,29 @@
+var mongoose = require('mongoose'),
+    Schema   = mongoose.Schema;
+
+var ReceiveMR = new Schema ({
+  _id : {
+    email: {type: String},
+    userId : {type: Schema.ObjectId}
+  },
+  value : {type: Number}
+})
+
+
+var SentAndCoReceiveMR = new Schema ({
+  _id : {
+    email: {type: String},
+    userId : {type: Schema.ObjectId}
+  },
+  value : {
+    sent : {type : Number},
+    corecipient : {type : Number}
+  }
+})
+
+
+ReceiveMR.index({ "_id.email": 1, "_id.userId": 1 }, {unique : true});
+SentAndCoReceiveMR.index({ "_id.email": 1, "_id.userId": 1 }, {unique : true});
+
+mongoose.model('ReceiveMR', ReceiveMR);
+mongoose.model('SentAndCoReceiveMR', SentAndCoReceiveMR);
