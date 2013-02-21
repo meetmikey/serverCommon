367f7b0b452ab3cbe2039cd9fbcbd07283fba4fb
Sagar Mehta <mehta.sagarv@gmail.com>
2013-02-20 21:16:48 -0800

    set up active connections schema

---
+++ schema/active.js
@@ -0,0 +1,12 @@
+var mongoose = require('mongoose');
+var Schema = mongoose.Schema;
+
+var ActiveConnection = new Schema({
+    userId: {type: Schema.ObjectId, required: true, index : true}
+  , lastPoll: {type: Date, default : Date.now}
+  , nodeId : {type : String, index : true}  // uniquely identifies which node process is handling the connection
+});
+
+ActiveConnection.index({ lastPoll: 1}, {expireAfterSeconds : constants.ACTIVE_CONNECTION_TTL });
+
+mongoose.model('ActiveConnection', ActiveConnection);
