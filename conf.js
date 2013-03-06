d6fe3023a7d2dc9088697f4dc6ab80b5d3692b2b
Sagar Mehta <mehta.sagarv@gmail.com>
2013-03-05 20:56:27 -0800

    change index name

--- conf.js
+++ conf.js
@@ -101,7 +101,7 @@ module.exports = {
   , elasticSearch: {
       host: 'localhost'
     , port: 9200
-    , indexName: 'mail_v1'
+    , indexName: 'mail'
     , indexAlias : 'mail'
     , mappingConfigs: [
       {
