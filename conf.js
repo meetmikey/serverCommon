32ba5aed39d5589dc3ab10983fa0a5965d5044be
Sagar Mehta <mehta.sagarv@gmail.com>
2013-03-06 01:24:08 -0800

    better logging in sqs connect, updated models for active conn and onboard to use common semantics

--- conf.js
+++ conf.js
@@ -101,7 +101,7 @@ module.exports = {
   , elasticSearch: {
       host: 'localhost'
     , port: 9200
-    , indexName: 'mail_v1'
+    , indexName: 'mail' // TODO: switch to mail_v1
     , indexAlias : 'mail'
     , mappingConfigs: [
       {
