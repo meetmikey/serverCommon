fcf968d9c870c9ce30433781407ab7417cbb9c8b
Sagar Mehta <mehta.sagarv@gmail.com>
2013-03-08 19:15:26 -0800

    updated filename on downloads

---
+++ test/testImageFromWeb.js
@@ -0,0 +1,33 @@
+var serverCommon = process.env.SERVER_COMMON;
+var azureUtils = require (serverCommon + '/lib/azureUtils')
+    , http = require ('http')
+    , conf = require (serverCommon + '/conf');
+
+
+azureUtils.downloadAndSaveStaticImage('http://1800hocking.files.wordpress.com/2011/07/hi-ohio-logo.jpg', function (err, path) {
+  if (err) {
+    console.error ('test failed', err);
+    return;
+  }
+
+  console.log ('path', path);
+});
+
+
+/*
+var blobService = azure.createBlobService(conf.azure.storageAccount, conf.azure.storageAccessKey);
+
+
+ http.get( 'http://1800hocking.files.wordpress.com/2011/07/hi-ohio-logo.jpg', function( response ) {
+    var contentLength = Number(response.headers['content-length']);
+
+    blobService.createBlockBlobFromStream (conf.azure.container, 'myImage', response, contentLength, function (err) {
+      console.log (err);
+
+    })
+  }).on( 'error', function( err ) {
+    console.error (err);
+  });
+
+
+*/
