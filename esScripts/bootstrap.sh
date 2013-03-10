23132733586696370960fd2f52690795ac832700
Sagar Mehta <mehta.sagarv@gmail.com>
2013-03-09 17:17:45 -0800

    bash script for bootstrap asks for confirmation

--- esScripts/bootstrap.sh
+++ esScripts/bootstrap.sh
@@ -1,4 +1,15 @@
-./deleteIndex.sh
-./createIndex.sh
-./createAlias.sh
-./putMapping.sh
+read -p "This will delete and recreate the index. ARE YOU SURE (y/n)?? " -n 1
+if [[ $REPLY =~ ^[Yy]$ ]]
+then
+  echo -e "\n"
+  ./deleteIndex.sh
+  echo -e "\n"
+  ./createIndex.sh
+  echo -e "\n"
+  ./createAlias.sh
+  echo -e "\n"
+  ./putMapping.sh
+  echo -e "\n"
+  echo -e "Goddamnit leeroy."
+  exit 1
+fi
