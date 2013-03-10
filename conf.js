95001b052e4f1391945e4dd278f041d750480b9f
Sagar Mehta <mehta.sagarv@gmail.com>
2013-03-09 19:44:08 -0800

    more on cloudstorage utils

--- conf.js
+++ conf.js
@@ -56,6 +56,7 @@ module.exports = {
       , static: 'images'
       , linkInfo: 'linkInfo'
       , mailBody : 'mailBody'
+      , rawEmail : 'rawEmail'
     }
   }
   , azure : {
@@ -67,6 +68,7 @@ module.exports = {
         , static: 'images'
         , linkInfo: 'linkInfo'
         , mailBody : 'mailBody'
+        , rawEmail : 'rawEmail'
       },
       sharedSecret : '40e807a00561eff916c10bcfa39183043ad8f632a458'
   }
