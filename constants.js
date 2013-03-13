3fbdf78c430502adf2bd9ed4d3c65327d5c682eb
Sagar Mehta <mehta.sagarv@gmail.com>
2013-03-12 17:32:45 -0700

    modify constants

--- constants.js
+++ constants.js
@@ -12,7 +12,7 @@ define('DEFAULT_RESPONSE_CODE', 500)
 define('LOG_BREAK', '\n\n\n\n\n\n\n\n');
 
 // in seconds
-define('ACTIVE_CONNECTION_TTL', 60*5) // 5 minutes
+define('ACTIVE_CONNECTION_TTL', 60*3) // 5 minutes
 
 define ('MAIL_READER_CHECK_INTERVAL', 1000*5);
 
@@ -22,7 +22,7 @@ define ('MAIL_DOWNLOAD_CHECK_INTERVAL', 1000*5);
 
 define ('MAIL_UPDATE_CHECK_INTERVAL', 1000*5);
 
-define ('ACTIVE_CONNECTIONS_CHECK_INTERVAL', 1000*2); // 2 minutes, we poll every minute so this should suffice
+define ('ACTIVE_CONNECTIONS_CHECK_INTERVAL', 1000*1); // the interval at which we re-check < threshold if mikeymail mode=listen is saturated
 
 define ('CLOUD_STORAGE_DEFAULT', 'aws');
 
