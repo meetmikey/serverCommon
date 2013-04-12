f9a2807ad621b5f08464d7f94b15d91154e073d1
Sagar Mehta <mehta.sagarv@gmail.com>
2013-04-12 01:54:17 -0700

    nearly there on indexingRefactor

--- schema/indexState.js
+++ schema/indexState.js
@@ -2,6 +2,7 @@ var indexStateSchema = {
   indexState : {type : String, enum : ['done', 'softFail', 'hardFail'], index : true}
   , indexError : {type : String}
   , tries : {type : Number, default : 0}
+  , version : {type : Number}
 };
 
 
