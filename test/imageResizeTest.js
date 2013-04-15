d8b63e3f82497ab23e152c6d7f15699a87e7dd25
Sagar Mehta <mehta.sagarv@gmail.com>
2013-04-14 18:19:24 -0700

    pull from out own gm module so that we put a timeout around spawned processes

--- test/imageResizeTest.js
+++ test/imageResizeTest.js
@@ -5,8 +5,8 @@ var imageUtils = require (serverCommon + '/lib/imageUtils')
     , fs = require ('fs');
 
 var imageResizeTest = this;
-//var testFiles = ['./test/data/fist.png', './test/data/image.tiff', './test/data/obama_emails.jpg', './test/data/needs_crop.png', './test/data/wide.png', './test/data/wide2.png'];
-var testFiles = ['./test/data/sisbanner_stream.jpg'];
+var testFiles = ['./test/data/fist.png', './test/data/image.tiff', './test/data/obama_emails.jpg', './test/data/needs_crop.png', './test/data/wide.png', './test/data/wide2.png'];
+//var testFiles = ['./test/data/sisbanner_stream.jpg'];
 
 exports.testBuffer = function (path, name, contentType) {
 
@@ -15,7 +15,7 @@ exports.testBuffer = function (path, name, contentType) {
 
     imageUtils.generateThumbnailForImagesTab ("test/buffer_" + name, data, true, contentType, function (err, needsThumb) {
       if (err) {
-        winston.handleError (err);
+        console.log (err);
       }
       else if (!needsThumb) {
         winston.info ('rollover thumbnail not generated because image is already small');
@@ -27,7 +27,7 @@ exports.testBuffer = function (path, name, contentType) {
 
     imageUtils.generateThumbnailForRollover ("test/buffer_rollover_" + name, data, true, contentType, function (err, needsThumb) {
       if (err) {
-        winston.handleError (err);
+        console.log (err);
       }
       else if (!needsThumb) {
         winston.info ('thumbnail not generated because image is already small');
@@ -47,7 +47,7 @@ exports.testStreaming = function (path, name, contentType) {
 
   imageUtils.generateThumbnailForImagesTab ("stream_" + name, rs, false, contentType, function (err, needsThumb) {
     if (err) {
-      winston.handleError (err);
+      console.log (err);
     }
     else if (!needsThumb) {
       winston.info ('thumbnail not generated because image is already small');
@@ -61,7 +61,7 @@ exports.testStreaming = function (path, name, contentType) {
 
   imageUtils.generateThumbnailForRollover ("stream_rollover_" + name, rs2, false, contentType, function (err, needsThumb) {
     if (err) {
-      winston.handleError (err);
+      console.log (err)
     }
     else if (!needsThumb) {
       winston.info ('thumbnail not generated because image is already small');
