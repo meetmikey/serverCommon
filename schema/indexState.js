7a4ed36c2c09ef16c33c38007440fcffd7312fc9
Sagar Mehta <mehta.sagarv@gmail.com>
2013-04-15 17:03:57 -0700

    max retries in indexing... sqs looks at winston field in err to decide whether to delete from queue anyway

--- schema/indexState.js
+++ schema/indexState.js
@@ -2,7 +2,8 @@ var indexStateSchema = {
   indexState : {type : String, enum : ['done', 'softFail', 'hardFail'], index : true}
   , indexError : {type : String}
   , tries : {type : Number, default : 0}
-  , version : {type : Number}
+  , version : {type : String}
+  , asStub : {type : Boolean} // after a number of failed indexing attempts we may keep a stub resource in the index with whatever info we could extract
 };
 
 
