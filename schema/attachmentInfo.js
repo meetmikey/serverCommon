1a662e6deb6c76e1b49b4b733ab96cdfce231fe3
Sagar Mehta <mehta.sagarv@gmail.com>
2013-04-22 15:07:02 -0700

    modify indexState schema

--- schema/attachmentInfo.js
+++ schema/attachmentInfo.js
@@ -21,7 +21,7 @@ var AttachmentInfo = new Schema({
                                       'archive',
                                       'other']}
   , hash: {type: String, required: true}
-  , index : indexStateSchema
+  , index : {type : [indexStateSchema], default :[]}
   , timestamp: {type: Date, default: Date.now}
 });
 
--- schema/indexState.js
+++ schema/indexState.js
@@ -1,10 +1,12 @@
-var indexStateSchema = {
+var mongoose = require('mongoose'),
+    Schema   = mongoose.Schema;
+
+var indexStateSchema = new Schema ({
   indexState : {type : String, enum : ['done', 'softFail', 'hardFail'], index : true}
-  , indexError : {type : String}
   , tries : {type : Number, default : 0}
   , version : {type : String}
   , asStub : {type : Boolean} // after a number of failed indexing attempts we may keep a stub resource...
-};
+});
 
 
 exports.indexStateSchema = indexStateSchema;
