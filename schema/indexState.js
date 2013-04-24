6d81ab2cd00dce975d82249e4eb37a55f251e9e4
Sagar Mehta <mehta.sagarv@gmail.com>
2013-04-24 12:15:18 -0700

    updated link threshold, removed an index

--- schema/indexState.js
+++ schema/indexState.js
@@ -2,7 +2,7 @@ var mongoose = require('mongoose'),
     Schema   = mongoose.Schema;
 
 var indexStateSchema = new Schema ({
-  indexState : {type : String, enum : ['done', 'softFail', 'hardFail'], index : true}
+  indexState : {type : String, enum : ['done', 'softFail', 'hardFail']}
   , tries : {type : Number, default : 0}
   , version : {type : String}
   , asStub : {type : Boolean} // after a number of failed indexing attempts we may keep a stub resource...
