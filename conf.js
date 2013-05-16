e444d1aad5ce5e3d321b2697d9b4897af69bff22
Justin Durack <justin@magicnotebook.com>
2013-05-15 20:47:17 -0700

    killed azure sharedSecret.  Not sure what it's even supposed to be.

--- conf.js
+++ conf.js
@@ -109,8 +109,7 @@ module.exports = {
         , linkInfo: 'linkInfo'
         , mailBody : 'mailBody'
         , rawEmail : 'rawEmail'
-      },
-      sharedSecret : '40e807a00561eff916c10bcfa39183043ad8f632a458'
+      }
   }
   , mongo: {
       local: {
