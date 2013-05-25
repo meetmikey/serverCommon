a09f695c7d45e4847f0c855414f540b0b3445f08
Sagar Mehta <mehta.sagarv@gmail.com>
2013-05-25 02:03:58 -0700

    daysLimit basic implementation - changed what index to use for mikeymail downloads

--- schema/mail.js
+++ schema/mail.js
@@ -52,8 +52,8 @@ var Mail = new Schema({
 });
 
 // for querying for attachments or no attachments + user and sorting by uid descending
-Mail.index({ userId: 1, s3Path : 1, hasAttachment: 1, uid : -1 } );
-Mail.index ({userId: 1, s3Path: 1, hasMarketingText : 1, hasMarketingFrom : 1, uid : -1});
+Mail.index({userId: 1, mmDone : 1, hasAttachment: 1, uid : -1 } );
+Mail.index ({userId: 1, mmDone: 1, hasMarketingText : 1, hasMarketingFrom : 1, uid : -1});
 Mail.index ({mmDone: 1, userId: 1, mailReaderState: 1});
 
 //db.mails.ensureIndex ({userId : 1, s3Path : 1, hasMarketingText : 1, hasMarketingFrom: 1, uid : -1})
