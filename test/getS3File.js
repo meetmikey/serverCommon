d2f0f56d7014b9ab416f432d8fc7461c498b6ca9
Justin Durack <justin@magicnotebook.com>
2013-05-29 21:25:02 -0700

    set and check 404 error type on s3 file downloads

--- test/getS3File.js
+++ test/getS3File.js
@@ -3,14 +3,18 @@ var serverCommon = process.env.SERVER_COMMON;
 var s3Utils = require(serverCommon + '/lib/s3Utils')
   , fs = require('fs')
   , winston = require(serverCommon + '/lib/winstonWrapper').winston
-  util = require(serverCommon + '/lib/utils')
+  , utils = require(serverCommon + '/lib/utils')
+  , constants = require(serverCommon + '/constants')
 
 var file = process.argv[2];
-winston.doInfo('file: ', {file: file});
+winston.doInfo('file2: ', {file: file});
 
 s3Utils.getFile (file, false, function (err, res) {
 
   if ( err ) {
+    if ( winston.getErrorType( err ) == constants.ERROR_TYPE_404 ) {
+      winston.doInfo('404 error!');
+    }
     winston.handleError(err);
 
   } else if ( ! res ) {
