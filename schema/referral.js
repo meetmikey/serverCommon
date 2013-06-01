1397aceee929723aee03616a8979abe55e93da7a
Justin Durack <justin@magicnotebook.com>
2013-05-31 18:07:13 -0700

    added referralUtils lib, schema, moved some constants.

---
+++ schema/referral.js
@@ -0,0 +1,20 @@
+var mongoose = require ('mongoose')
+  , Schema = mongoose.Schema
+  , constants = require('../constants')
+
+var Referral = new Schema({
+    oldUserId: {type: Schema.ObjectId}
+  , newUserId: {type: Schema.ObjectId}
+  , timestamp: {type: Date, default: Date.now}
+  , source: {type: String, enum: [
+      constants.REFERRAL_SOURCE_TWITTER
+    , constants.REFERRAL_SOURCE_FACEBOOK
+    , constants.REFERRAL_SOURCE_DIRECT
+    ]}
+  , originURL: {type: String}
+});
+
+Referral.index({oldUserId: 1, newUserId: 1}, {unique: true});
+
+mongoose.model('Referral', Referral);
+exports.ReferralModel = mongoose.model('Referral');
