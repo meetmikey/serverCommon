27c6d7769b3d858853cebfd16c4a85358f00bf44
Sagar Mehta <mehta.sagarv@gmail.com>
2013-07-22 23:21:29 -0700

    automate all mail error

--- schema/user.js
+++ schema/user.js
@@ -40,7 +40,8 @@ var User = new Schema({
   minMailDate : {type : Date}, // the date of the earliest mail in the gmail account
   daysLimit : {type : Number, default : constants.BASE_DAYS_LIMIT}, // how many days the user is entitled to
   isPremium : {type : Boolean, default : false}, // flag to "ignore" the daysLimit in the account entirely
-  allMailError : {type : Boolean}
+  allMailError : {type : Boolean},
+  allMailOnboardAttempts : {type : Number, default : 0}
 }, schemaOptions );
 
 // virtual fields for specialized links
