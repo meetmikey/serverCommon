9c354c627fb744f460031285012c31c00690c585
Justin Durack <justin@magicnotebook.com>
2013-07-25 17:13:33 -0700

    refactored, reworked user upgrading code

--- schema/user.js
+++ schema/user.js
@@ -40,6 +40,7 @@ var User = new Schema({
   minMailDate : {type : Date}, // the date of the earliest mail in the gmail account
   daysLimit : {type : Number, default : constants.BASE_DAYS_LIMIT}, // how many days the user is entitled to
   isPremium : {type : Boolean, default : false}, // flag to "ignore" the daysLimit in the account entirely
+  isGrantedPremium : {type : Boolean}, // flag to indicate that we manually granted this user a 'premium' account
   allMailError : {type : Boolean},
   stripeCustomerId: {type: String},
   billingPlan: {type: String, enum: ['free', 'basic', 'pro']},
