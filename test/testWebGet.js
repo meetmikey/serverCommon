9fd4b9bad0c48a1bb1b114ea7e691c3b2301979e
Sagar Mehta <mehta.sagarv@gmail.com>
2013-08-12 21:09:13 -0700

    added earlier checks for too big response based on response headers where relevant

--- test/testWebGet.js
+++ test/testWebGet.js
@@ -7,7 +7,8 @@ var mongoose = require (serverCommon + '/lib/mongooseConnect').mongoose
 //var url = 'https://github.com/JomuMist/VacationRentals/commit/a5d0a77db82c5e5e3b9d3e94f7ebf3de95331af4'
 //var url = 'http://mailstat.us/tr/t/p2yekqgmbo6rhhifw6i9/3/https://www.vizify.com/es/51633c8f42ba9b000e000412'
 //var url = 'http://www.espn.go.com'
-var url = 'http://www.nytimes.com/2013/05/19/magazine/why-basketball-wont-leave-phil-jackson-alone.html'
+var url = 'https://dl.dropboxusercontent.com/u/15598695/%E4%BA%BA%E6%89%8D%E5%9F%8E_New.pdf'
+//var url = 'http://www.nytimes.com/2013/05/19/magazine/why-basketball-wont-leave-phil-jackson-alone.html'
 
 webUtils.webGet( url, true, function (err, response, url, headers )  {
   winston.doInfo('info', {err: err, url: url, headers: headers});
