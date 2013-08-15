78b0c8858f95571d789402c51ca17dafd89a80bf
Sagar Mehta <mehta.sagarv@gmail.com>
2013-08-15 00:57:46 -0700

    indexingHandler webGet takes extra argument, code cleanup, make constant 30 mb

--- constants.js
+++ constants.js
@@ -51,9 +51,7 @@ define ('MAX_INDEXING_ATTEMPTS', 4);
 
 define ('MAX_NGINX_TIMEOUTS', 3);
 
-//define('MAX_STREAM_TO_BUFFER', 31457280);
-
-define('MAX_STREAM_TO_BUFFER', 100);
+define('MAX_STREAM_TO_BUFFER', 31457280);
 
 define('ERROR_TYPE_404', '404');
 
