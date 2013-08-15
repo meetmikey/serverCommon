5e327ef63681a8b35961d49e5348c6cb5a4644d5
Sagar Mehta <mehta.sagarv@gmail.com>
2013-08-14 23:55:04 -0700

    fixed up more things, closer to functional buffer cap with stub linkinfo indexing

--- test/testWebGet.js
+++ test/testWebGet.js
@@ -8,8 +8,10 @@ var mongoose = require (serverCommon + '/lib/mongooseConnect').mongoose
 //var url = 'http://mailstat.us/tr/t/p2yekqgmbo6rhhifw6i9/3/https://www.vizify.com/es/51633c8f42ba9b000e000412'
 //var url = 'http://www.espn.go.com'
 var url = 'https://dl.dropboxusercontent.com/u/15598695/%E4%BA%BA%E6%89%8D%E5%9F%8E_New.pdf'
+var url = 'https://www.dropbox.com/s/e1gyvy4ymsgkkrk/Tech%20Talk%20Slides.pptx'
 //var url = 'http://www.nytimes.com/2013/05/19/magazine/why-basketball-wont-leave-phil-jackson-alone.html'
 
 webUtils.webGet( url, true, function (err, response, url, headers )  {
   winston.doInfo('info', {err: err, url: url, headers: headers});
+  console.log (response)
 });
