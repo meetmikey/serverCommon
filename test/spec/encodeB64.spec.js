241e23421aa0d22b18ba85c047f399885db2eb2b
Justin Durack <justin@meetmikey.com>
2013-08-25 21:40:05 -0700

    fixed encoding function

--- test/spec/encodeB64.spec.js
+++ test/spec/encodeB64.spec.js
@@ -9,16 +9,16 @@ describe('encodeB64', function() {
     var encoded = utils.encodeB64( data );
     expect( encoded ).toBe( expectedEncoded );
 
-    data = {"event": "e-mail opened", 
+    data = {"event": "e-mail opened",
       "properties": {
-        "distinct_id": "user@mixpanel.com", 
-        "token": "e3bc4100330c35722740fb8c6f5XXXXX", 
-        "time": 1245613885, 
+        "distinct_id": "user@mixpanel.com",
+        "token": "e3bc4100330c35722740fb8c6f5XXXXX",
+        "time": 1245613885,
         "campaign": "employee wellness"
         }
     }
-    expectedEncoded = 'eyJldmVudCI6ICJnYW1lIiwgInByb3BlcnRpZXMiOiB7ImlwIjogIjEyMy4xMjMuMTIzLjEyMyIsICJ0b2tlbiI6ICJlM2JiNDEwMDMzMGMzNTcyMjc0MGZiOGM2ZjVhYmRkYyIsICJ0aW1lIjogMTI0NTYxMzg4NSwgImFjdGlvbiI6ICJwbGF5In19';
+    expectedEncoded = 'eyJldmVudCI6ImUtbWFpbCBvcGVuZWQiLCJwcm9wZXJ0aWVzIjp7ImRpc3RpbmN0X2lkIjoidXNlckBtaXhwYW5lbC5jb20iLCJ0b2tlbiI6ImUzYmM0MTAwMzMwYzM1NzIyNzQwZmI4YzZmNVhYWFhYIiwidGltZSI6MTI0NTYxMzg4NSwiY2FtcGFpZ24iOiJlbXBsb3llZSB3ZWxsbmVzcyJ9fQ==';
     encoded = utils.encodeB64( data );
-    //expect( encoded ).toBe( expectedEncoded ); //not sure my comparison value is actually correct here.
+    expect( encoded ).toBe( expectedEncoded ); //not sure my comparison value is actually correct here.
   });
 });
