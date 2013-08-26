c27eefd339ad294edc37d2b5ce8bf02fd3e6fe40
Sagar Mehta <mehta.sagarv@gmail.com>
2013-08-26 14:50:06 -0700

    have a test case for connecting to the index

--- test/esConnect.js
+++ test/esConnect.js
@@ -1,1 +1,8 @@
-var esConnect = require('../lib/esConnect')
+var SERVER_COMMON = process.env.SERVER_COMMON;
+
+var esConnect = require('../lib/esConnect');
+var conf = require (SERVER_COMMON + '/conf');
+
+esConnect.init(conf, function (err) {
+  console.log (err);
+})
