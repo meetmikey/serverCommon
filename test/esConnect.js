127d844568913c6a9bea6fe4cf32ddaf1d84bc72
Sagar Mehta <mehta.sagarv@gmail.com>
2013-08-26 18:36:36 -0700

    turn debug mode on

--- test/esConnect.js
+++ test/esConnect.js
@@ -2,6 +2,7 @@ var SERVER_COMMON = process.env.SERVER_COMMON;
 
 var esConnect = require('../lib/esConnect');
 var conf = require (SERVER_COMMON + '/conf');
+conf.turnDebugModeOn();
 
 esConnect.init(conf, function (err) {
   console.log (err);
