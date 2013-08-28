73a2d765f458ab0e3c274ed3c2b7809bbceadc15
Sagar Mehta <mehta.sagarv@gmail.com>
2013-08-27 17:26:04 -0700

    added lastLogin field

--- schema/user.js
+++ schema/user.js
@@ -50,7 +50,8 @@ var User = new Schema({
   billingPlan: {type: String, enum: ['free', 'basic', 'pro', 'team'], default: 'free'},
   billingPlanStartDate: {type: Date},
   billingPlanEndDate: {type: Date},
-  lastResumeJobEndDate : {type : Date, default: Date.now}
+  lastResumeJobEndDate : {type : Date, default: Date.now},
+  lastLogin : {type : Date}
 }, schemaOptions );
 
 // virtual fields for specialized links
