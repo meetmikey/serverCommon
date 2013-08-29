632de69a165099b162bf5d95df4cdadbb9251efc
Sagar Mehta <mehta.sagarv@gmail.com>
2013-08-28 23:24:25 -0700

    header parsing done by mimelib.. not me.

--- test/testContactExtract.js
+++
@@ -1,7 +0,0 @@
-var mailUtils = require ('../lib/mailUtils')
-  , winston = require ('../lib/winstonWrapper').winston
-
-var hdrs = {to : '"Bradfield, David" <david.bradfield@fleishman.ca>, Cheesan Chew <cchew@ideacouture.com>, Andrew Lockhart <alockhart@ideacouture.com>'}
-
-var normalizedHeaders = mailUtils.normalizeAddressArrays(hdrs);
-winston.doInfo('normalizedHeaders', {normalizedHeaders: normalizedHeaders});
