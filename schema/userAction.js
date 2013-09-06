78dbee72a3d37068c726d13167a0b152a4bbf8e4
Justin Durack <justin@meetmikey.com>
2013-09-05 18:13:46 -0700

    started work on incentiveDays

---
+++ schema/userAction.js
@@ -0,0 +1,15 @@
+var constants = require ('../constants')
+  , mongoose = require('mongoose')
+  , Schema = mongoose.Schema
+
+var UserAction = new Schema({
+    userId: {type: Schema.ObjectId, required: true}
+  , action: {type : String, enum: ['favorite', 'like'], required: true}
+  , resourceId: {type: Schema.ObjectId, required: true}
+  , resourceType: {type : String, enum: ['attachment', 'image'], required: true},
+});
+
+UserAction.index( {userId: 1, action: 1, resourceId: 1, resourceType: 1}, {unique: true} );
+
+mongoose.model('UserAction', UserAction);
+exports.UserActionModel = mongoose.model('UserAction');
